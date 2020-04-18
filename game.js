71db7ff15645b5ed4db84e477990adaeae130fc3
XiaoGeNintendo <34835642+XiaoGeNintendo@users.noreply.github.com>
2020-04-18 23:11:31 +0800

    Create game.js

---
+++ game.js
@@ -0,0 +1,11 @@
+var template={
+    name:"Game Title",
+    rating:10,
+    status:[["code","msg"],["code","msg"]],
+    comment:"Leave your comment here",
+    get:"How do i get this game",
+    platform:"code of platform",
+    lang:"code of lang",
+    timestamp:"2020/4/18 some timestamp and description text",
+    type:"ACT?"
+}
